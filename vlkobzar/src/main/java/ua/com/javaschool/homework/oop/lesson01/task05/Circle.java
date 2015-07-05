c99853067299075252434be2fad68a76da51f12a
Razbok <otis_13@ukr.net>
2015-07-05 23:06:38 +0300

    Add lesson 01 task 05

---
+++ vlkobzar/src/main/java/ua/com/javaschool/homework/oop/lesson01/task05/Circle.java
@@ -0,0 +1,42 @@
+package Lesson_1.Circ;
+
+import java.util.Scanner;
+
+/**
+ * Написать класс Circle (круг) по аналогии с
+ Rectangle и метод, который будет возвращать его
+ площадь.
+ */
+public class Circle {
+    public static final String NAME = "Circle";
+    private double r, pi;
+
+    public Circle(double r, double pi){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pi = 3.14");
+        System.out.println("Enter radius:");
+        this.r = sc.nextInt();
+        this.pi = 3.14;
+    }
+
+    public void setPi(double pi) {
+        this.pi = pi;
+    }
+
+    public double getPi() {
+        return pi;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double square(){
+        return r * r * pi;
+    }
+}
---
+++ vlkobzar/src/main/java/ua/com/javaschool/homework/oop/lesson01/task05/Main.java
@@ -0,0 +1,12 @@
+package Lesson_1.Circ;
+
+/**
+ * Created by Êמבחאנ on 02.07.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Circle.NAME);
+        Circle c = new Circle(0,0);
+        System.out.println("Square circle: " + c.square());
+    }
+}
